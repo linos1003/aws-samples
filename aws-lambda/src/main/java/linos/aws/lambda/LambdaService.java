166c2a756cb42478d0e5f0daf605c41ab1d7d940
Linos <linos@Bassems-MacBook-Pro.local>
2018-08-07 16:42:57 +0200

    first attempt with cloudformation

--- aws-lambda/src/main/java/linos/aws/lambda/LambdaService.java
+++ aws-lambda/src/main/java/linos/aws/lambda/LambdaService.java
@@ -3,7 +3,7 @@ package linos.aws.lambda;
 import com.amazonaws.services.lambda.invoke.LambdaFunction;
 
 public interface LambdaService {
-    @LambdaFunction(functionName="test")
+    @LambdaFunction(functionName="test2")
     String helloFromLabda(Object input);
 
 }
