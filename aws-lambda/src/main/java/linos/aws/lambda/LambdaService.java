5be66314a5d4ef1989c4732bc0c1b92992e9c3ab
Linos <linos@Bassems-MacBook-Pro.local>
2018-08-08 22:12:28 +0200

    delete .idea

--- aws-lambda/src/main/java/linos/aws/lambda/LambdaService.java
+++ aws-lambda/src/main/java/linos/aws/lambda/LambdaService.java
@@ -3,7 +3,7 @@ package linos.aws.lambda;
 import com.amazonaws.services.lambda.invoke.LambdaFunction;
 
 public interface LambdaService {
-    @LambdaFunction(functionName="test2")
+    @LambdaFunction(functionName="test-with-cloudformation")
     String helloFromLabda(Object input);
 
 }
